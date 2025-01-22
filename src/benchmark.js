2b083fa4b3b419f3bbc6e31b9c26fa85622c61e9
Jack Crane <jack@jackcrane.rocks>
2025-01-22 06:30:29 -0600

    Added (commented out) save the img on fail for debugging

--- src/benchmark.js
+++ src/benchmark.js
@@ -1,21 +1,27 @@
-import e from "express";
 import { findNearestVector } from "./findNearestVector.js";
 import { generateLetterVector } from "./generateLetterVector.js";
 import { loadTest } from "./load.js";
 import chalk from "chalk";
+import { generateLetterImage } from "./generateLetterImage.js";
 
 const mnist = loadTest();
 
 let successCount = 0;
 let failCount = 0;
 
-for (let i = 0; i < 10000; i++) {
+for (let i = 0; i < mnist.length; i++) {
   const imageData = mnist[i];
   const vector = generateLetterVector(imageData);
 
   const nearest = findNearestVector(vector.vector);
 
   if (nearest.nearestVector.character !== vector.character) {
+    // Render and save the failed image
+    // generateLetterImage(imageData, {
+    //   writeImage: true,
+    //   idx: `failed-${i}-found-${nearest.nearestVector.character}-expected-${vector.character}`,
+    // });
+
     failCount++;
     console.log(
       chalk.redBright("FAIL"),
